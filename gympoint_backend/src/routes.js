28d285c370a7cf7857e8a5aee6a1de125559ef7b
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-10-21 14:36:54 -0300

    Configura a rota basica de hello world dentro do servidor

--- gympoint_backend/src/routes.js
+++ gympoint_backend/src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/',(req, res) => {
-  return res.json({"message": "Hello World"});
+routes.get('/', (req, res) => {
+  return res.json({"message": "olá mundo"});
 });
 
 export default routes;
