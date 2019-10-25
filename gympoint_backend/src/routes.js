da71d14799308b3a1c85e8b48d0e6d39378607cc
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-10-25 10:08:35 -0300

    Configura controller para a sessão do usuário e conecta com a rota especifica

--- gympoint_backend/src/routes.js
+++ gympoint_backend/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionCrontroller';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'olá mundo' }));
+routes.post('/sessions', SessionController.store);
 
 export default routes;
