10e73813d726b5eb12e768b305b83052152d41af
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-10-29 17:42:28 -0300

    Cria rota que cadastra usuário, ainda sem validações

--- gympoint_backend/src/app/controllers/StudentController.js
+++ gympoint_backend/src/app/controllers/StudentController.js
@@ -1,1 +1,23 @@
 import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const { email } = req.body;
+
+    const student = await Student.findOne({ where: { email } });
+
+    if (student) {
+      return res.status(400).json({ error: 'Student already registered!' });
+    }
+
+    const { id, name } = await Student.create(req.body);
+
+    return res.status(201).json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new StudentController();
