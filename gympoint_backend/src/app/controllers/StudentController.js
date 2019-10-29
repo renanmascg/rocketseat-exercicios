73c6e9b3618c6badd4da0bea3f3c2141fd7d9133
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-10-29 17:59:59 -0300

    Adiciona validação de dados de entrada para Student

--- gympoint_backend/src/app/controllers/StudentController.js
+++ gympoint_backend/src/app/controllers/StudentController.js
@@ -1,7 +1,29 @@
+import * as Yup from 'yup';
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .integer()
+        .positive()
+        .required(),
+      weight: Yup.number()
+        .positive()
+        .required(),
+      height: Yup.number()
+        .positive()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email } = req.body;
 
     const student = await Student.findOne({ where: { email } });
